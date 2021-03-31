ec343de7a9c9b74f68d2f74ee72fbfa268afff37
Nicholas Baldwin <nicholas.baldwin@icloud.com>
2021-03-31 21:34:29 +0100

    Added #delete method

--- node.rb
+++ node.rb
@@ -3,8 +3,7 @@
 # Class defining a node for use with binary search trees.
 class Node
   include Comparable
-  attr_reader :data
-  attr_accessor :left_child, :right_child
+  attr_accessor :data, :left_child, :right_child
 
   def initialize(data)
     @data = data
--- tree.rb
+++ tree.rb
@@ -33,12 +33,44 @@ class Tree
     return Node.new(value) if root.nil?
     if root.data == value
       root
-    elsif root.data > value
+    elsif value < root.data
       root.left_child = insert(value, root.left_child)
-    else root.data < value
+    else value > root.data
       root.right_child = insert(value, root.right_child)
     end
     root
+  end
+
+  def delete(value, root = @root)
+    return root if root.nil?
+
+    if value < root.data
+      root.left_child = delete(value, root.left_child)
+    elsif value > root.data
+      root.right_child = delete(value, root.right_child)
+    else
+      if root.left_child.nil?
+        tmp = root.right_child
+        root = nil
+        return tmp
+      elsif root.right_child.nil?
+        tmp = root.left_child
+        root = nil
+        return tmp
+      end
+      tmp = min_value_node(root.right_child)
+      root.data = tmp.data
+      root.right_child = delete(tmp.data, root.right_child)
+    end
+    root
+  end
+
+  def min_value_node(node)
+    current = node
+    unless current.left_child.nil?
+      current = current.left_child
+    end
+    current
   end
 end
 
@@ -47,5 +79,9 @@ tree = Tree.new([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
 tree.pretty_print
 
 tree.insert(20)
+
+tree.pretty_print
+
+tree.delete(4)
 
 tree.pretty_print
